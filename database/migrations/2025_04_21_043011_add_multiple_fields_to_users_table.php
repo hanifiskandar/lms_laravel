9787290c658a1afa2f4b60ee5b76421ad2558efd
Muhammad Hanif <muhammad.hanif@inteksoft.com.my>
2025-04-21 16:49:51 +0800

    usee table

--- database/migrations/2025_04_21_043011_add_multiple_fields_to_users_table.php
+++ database/migrations/2025_04_21_043011_add_multiple_fields_to_users_table.php
@@ -25,3 +25,36 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
+// {
+//         Schema::table('users', function (Blueprint $table) {
+//             $table->dropColumn([
+//                 'username',
+//                 'nric',
+//                 'dob',
+//                 'marital_status',
+//                 'mobile_phone',
+//                 'office_phone',
+//                 'designation',
+//                 'position_level',
+//                 'department_id',
+//                 'address_line1',
+//                 'address_line2',
+//                 'address_line3',
+//                 'city',
+//                 'state',
+//                 'postcode',
+//                 'country',
+//                 'spouse_name',
+//                 'spouse_nric',
+//                 'spouse_job',
+//                 'spouse_employer',
+//                 'marital_status',
+//                 'date_of_birth',
+//                 'address',
+//                 'emergency_contact',
+//                 'is_active',
+//             ]);
+//         });
+//     }
+
